ce47082e51f1d0bbd0fdb05619da7140c93085e5
Paulo Ewerton <pauloewerton@icloud.com>
2022-12-21 13:43:15 -0300

    Aula 16

---
+++ aula16/src/config/parser.js
@@ -0,0 +1,13 @@
+const multer = require('multer');
+
+const parser = multer({
+  storage: multer.diskStorage({
+    destination: 'public/imgs',
+    filename(req, file, callback) {
+      file.key = `${randomBytes(16).toString('hex')}-${file.originalname}`;
+      callback(null, file.key);
+    },
+  }),
+});
+
+module.exports = parser;
